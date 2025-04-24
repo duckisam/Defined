d14e392b13b200ca6e6b81729040e69ba57d8fb0
duckisam <145405523+duckisam@users.noreply.github.com>
2025-04-24 10:50:13 -0500

    init

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <C:\curtis\Define\include\nlohmann\json.hpp>
+
+using json = nlohmann::json;
+
+std::string define(std::string word){
+    return word;
+}
 
 int main(){
-    std::cout << "Hello World";
+    std::cout << define("Hello");
     return 0;
 }
+
+
