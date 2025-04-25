40c6a3016f18eb635c48dfd9afdd197186378c69
duckisam <145405523+duckisam@users.noreply.github.com>
2025-04-25 09:43:52 -0500

    adding json defining any word

--- main.cpp
+++ main.cpp
@@ -1,15 +1,41 @@
 #include <iostream>
-#include <C:\curtis\Define\include\nlohmann\json.hpp>
+#include <fstream>
+#include <cctype>
+#include <string>
+#include <nlohmann\json.hpp>
 
 using json = nlohmann::json;
 
-std::string define(std::string word){
-    return word;
-}
+std::string lowerString(std::string word);
+std::string difine(std::string word);
+
 
 int main(){
-    std::cout << define("Hello");
+    std::cout << difine("World");
     return 0;
 }
 
+std::string difine(std::string word){
+    word = lowerString(word);
+    std::string fileName = "wordset-dictionary/data/" + std::string(1, word[0]) + ".json";
+    std::ifstream data(fileName);
+    
+    json wordDict;
 
+    data >> wordDict;
+    
+    std::string def = wordDict["world"]["meanings"][0]["def"];
+
+
+    data.close();
+
+    return def;
+}
+
+
+std::string lowerString(std::string word){
+    for(std::__cxx11::basic_string<char>::size_type i = 0; i < word.length(); i++){
+        word[i] = tolower(word[i]);
+    }
+    return word;
+}
