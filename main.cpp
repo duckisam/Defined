ae9b2ea331198528c5eea8daa177d7ad559dac53
duckisam <145405523+duckisam@users.noreply.github.com>
2025-04-25 12:55:50 -0500

    added input into the exe

--- main.cpp
+++ main.cpp
@@ -1,32 +1,32 @@
 #include <iostream>
 #include <fstream>
-#include <cctype>
 #include <string>
 #include <nlohmann\json.hpp>
 
 using json = nlohmann::json;
 
 std::string lowerString(std::string word);
-std::string difine(std::string word);
+std::string define(std::string word);
+bool isInString(char c, std::string inString);
 
-
-int main(){
-    std::cout << difine("World");
+int main(int argc, char *argv[]){
+    if(argc <= 1){
+        throw std::runtime_error("No Word Given");
+    }
+    
+    std::string word = argv[1];
+    std::string toReturn = define(word);
+    
     return 0;
 }
 
-std::string difine(std::string word){
+std::string define(std::string word){
     word = lowerString(word);
     std::string fileName = "wordset-dictionary/data/" + std::string(1, word[0]) + ".json";
     std::ifstream data(fileName);
-    
     json wordDict;
-
     data >> wordDict;
-    
-    std::string def = wordDict["world"]["meanings"][0]["def"];
-
-
+    std::string def = wordDict[word]["meanings"][0]["def"];
     data.close();
 
     return def;
@@ -34,8 +34,17 @@ std::string difine(std::string word){
 
 
 std::string lowerString(std::string word){
-    for(std::__cxx11::basic_string<char>::size_type i = 0; i < word.length(); i++){
-        word[i] = tolower(word[i]);
+    for(char& c : word){
+        c = std::tolower(c);
     }
     return word;
+}
+
+bool isInString(char c, std::string inString){
+    for(char i : inString){
+        if(c == i){
+            return true;
+        }
+    }
+    return false;
 }
